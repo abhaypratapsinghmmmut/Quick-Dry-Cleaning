240c7a3f2ed1ef3f635a31b365cc844fd5c81eb6
abhaypratapsinghmmmut <166241880+abhaypratapsinghmmmut@users.noreply.github.com>
2026-06-18 23:05:48 +0530

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   await app.listen(3001);
-  // eslint-disable-next-line no-console
+
+  
   console.log('Server running on http://localhost:3001');
 }
 
